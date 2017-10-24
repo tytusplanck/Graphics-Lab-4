86cd8a51272e559d79e842c5a5b8a8ec5b2345ae
Tytus Planck <tytusplanck@gmail.com>
2017-10-24 12:13:21 -0400

    Initial files for wall and manager.

---
+++ Lab 4/Assets/Scripts/BorderWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
