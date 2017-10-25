5ba8e86ec773d21ff9d377aea53df2586f252e55
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2017-10-25 14:04:17 -0400

    Our objects use force now

--- Lab 4/Assets/Scripts/Prey.cs
+++ Lab 4/Assets/Scripts/Prey.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class Prey : MonoBehaviour {
 
 	private Vector3 velocity;
+	Rigidbody rb;
+	float thrust = 1f;
 	// Use this for initialization
 	void Start () {
+
+		float thrust = Random.Range (-.000000002f, .000000002f);
 		transform.parent = transform;
-		transform.localPosition = new Vector3(Random.Range(0, 0), 0, 0);
-		velocity = new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1));
+		transform.localPosition = new Vector3(Random.Range(-19, 19), 0, Random.Range(-19, 19));
+		//velocity = new Vector3(Random.Range(-.002f, .002f), 0, Random.Range(-.002f, .002f));
+		rb.AddForce(transform.forward * thrust);
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.localPosition = transform.localPosition + velocity;
+		Vector3 rotation = transform.rotation.eulerAngles;
+		rotation.z = 2;
+		transform.rotation = Quaternion.Euler (rotation);
+
+		if (transform.position.x > 20 || transform.position.x < 20) {
+			rb.AddForce (transform.forward * thrust * -1f);
+		}
 	}
 }
