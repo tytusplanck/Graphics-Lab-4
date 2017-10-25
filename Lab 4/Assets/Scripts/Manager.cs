fb565c41630ef37e954b72bd7d831f30f95b00f1
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2017-10-25 09:26:24 -0400

    Created prey class

--- Lab 4/Assets/Scripts/Manager.cs
+++ Lab 4/Assets/Scripts/Manager.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour {
 
+	private Prey prey;
+	public Prey preyPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+		//int count = 0;
+		//while (count < 2) {
+			prey = Instantiate (preyPrefab) as Prey;
+		//	count = count + 1;
+		//}
+
 	}
 	
 	// Update is called once per frame
---
+++ Lab 4/Assets/Scripts/Prey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prey : MonoBehaviour {
+
+	public GameObject prey;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = prey.GetComponent<Rigidbody> ();
+		prey.transform.parent = transform;
+		prey.transform.localPosition = new Vector3(Random.Range(0, 100), 0, 0);
+		rb.AddForce (100, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
